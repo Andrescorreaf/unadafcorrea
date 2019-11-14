4c61a0ee6fa0d62fbcf7b8ef77b741ad6b678185
Andres Corres <felipefranco989@gmail.com>
2019-11-14 01:13:27 -0500

    Actualizacion de archivos

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
 // Empty JS for your own code to be here
+var lista = document.getElementById("lista1");
+lista.innerHTML += "<li>$ yum install git-core</li>";
+lista.innerHTML += "<li>$ yum install git-core</li>";
+
+
+
